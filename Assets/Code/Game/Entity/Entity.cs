d9af4ec875e5d8615a914ced03a1b7c13004cd50
casteillet <richardchristmann@hotmail.fr>
2026-03-11 10:46:44 +0100

    Fix Self error on enum in Entity

--- Assets/Code/Game/Entity/Entity.cs
+++ Assets/Code/Game/Entity/Entity.cs
@@ -1,15 +1,16 @@
 using KBCore.Refs;
 using UnityEngine;
+using VInspector;
 
 [SelectionBase]
 public class Entity : ValidatedMonoBehaviour
 {
     public EntityData EntityData { get; private set; }
     public CommandInvoker CommandInvoker { get; private set; }
-    
-    [field: SerializeField, Self] public EntityThreat Threat { get; private set; }
+
     [field: SerializeField, Self] public PathFollower PathFollower { get; private set; }
     [field: SerializeField, Self] public Health Health { get; private set; }
+    [field: SerializeField, ReadOnly] public EntityThreat Threat { get; private set; }
 
     public void Initialize(EntityData data, EntityThreat threat)
     {
